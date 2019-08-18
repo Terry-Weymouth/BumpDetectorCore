296f81528d8ed936277568d7717e6588b7eb2d70
Terry Weymouth <weymouth@umich.edu>
2019-08-18 12:46:56 -0400

    Updated test for serial connection - e.g. connection to GPS

--- python/serial_test.py
+++ python/serial_test.py
@@ -1,9 +1,9 @@
 import serial
-from datetime import datetime
-ser = serial.Serial('/dev/ttyUSB0',9600)
-bytes_second = 0
-lines = 0
-start = datetime.now()
+# Note for version one - using USB connection
+# device = '/dev/ttyUSB0'
+baud = 9600
+device = '/dev/ttys0'
+ser = serial.Serial(device,9600)
 while True:
     line=ser.readline()
     line = line.strip()
