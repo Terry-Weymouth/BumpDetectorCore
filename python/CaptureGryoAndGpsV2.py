a242189e4138715cbaee018eadce33e1cfd577ef
Terry Weymouth <weymouth@umich.edu>
2019-08-19 10:18:01 -0400

    Initial version of code for Version two

---
+++ python/CaptureGryoAndGpsV2.py
@@ -0,0 +1,43 @@
+# Parts of the code, copy modified from:
+#   | http://www.danmandle.com/blog/getting-gpsd-to-work-with-python/
+#   | Written by Dan Mandle http://dan.mandle.me September 2012
+#   | License: GPL 2.0
+ 
+from gps import *
+import threading
+ 
+gpsd = None #seting the global variable
+ 
+class GpsPoller(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        global gpsd  #bring it in scope
+        gpsd = gps(mode=WATCH_ENABLE)  # starting the stream of info
+        self.current_value = None
+        self.running = True  # setting the thread running to true
+ 
+    def run(self):
+        global gpsd
+        while gpsp.running:
+            gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+
+
+if __name__ == '__main__':
+    gpsp = GpsPoller() # create the thread
+    try:
+        gpsp.start() # start it up
+        while True:
+            more_line = "{},{},{},{},{}".format(
+                gpsd.utc,
+                gpsd.fix.latitude,
+                gpsd.fix.longitude,
+                gpsd.fix.altitude,
+                gpsd.fix.speed)
+            line = more_line
+            print(line)
+
+    except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+        print("\nKilling Thread...")
+        gpsp.running = False
+        gpsp.join() # wait for the thread to finish what it's doing
+    print("Done.\nExiting.")
